ae535922232fbb485f80086a9d275a253468ffe8
Luca Giovenzana <luca@giovenzana.org>
2021-05-04 19:35:05 +0200

    First experiment

---
+++ foreground.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="foreground" tilewidth="32" tileheight="32" tilecount="5766" columns="93">
+ <image source="foreground-original.png" width="2976" height="1984"/>
+</tileset>

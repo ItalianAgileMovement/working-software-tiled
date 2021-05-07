657ed5f53154a526de6931f86d6ea36d0a411887
Alessandro D'Amico <damico_alex@hotmail.com>
2021-05-07 20:10:40 +0200

    Add bar

---
+++ bar-background-original.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.5.0" name="bar-background-original" tilewidth="32" tileheight="32" tilecount="1200" columns="30">
+ <image source="bar-background-original.png" width="960" height="1280"/>
+</tileset>

9a842c673d47938523819edb7943604f151a3a1b
Alessandro D'Amico <damico_alex@hotmail.com>
2021-06-03 13:58:21 +0200

    add colors

---
+++ colors.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.5.0" name="colors" tilewidth="32" tileheight="32" tilecount="147" columns="21">
+ <image source="colors.PNG" width="678" height="229"/>
+</tileset>

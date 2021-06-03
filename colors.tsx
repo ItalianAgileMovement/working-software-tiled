bd43a0012fee17df4a770aed6bb8e8b55aa8c577
Damiano Salvi <salvi@intre.it>
2021-06-03 14:59:35 +0200

    Add room colors and names

--- colors.tsx
+++ colors.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.5.0" name="colors" tilewidth="32" tileheight="32" tilecount="147" columns="21">
- <image source="colors.PNG" width="678" height="229"/>
+<tileset version="1.5" tiledversion="1.6.0" name="colori-sorgenti" tilewidth="32" tileheight="32" tilecount="480" columns="40">
+ <image source="colori-sorgenti.png" width="1280" height="384"/>
 </tileset>
---
+++ letters.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.6.0" name="letters" tilewidth="32" tileheight="32" tilecount="30" columns="6">
+ <image source="scrabble.png" width="192" height="160"/>
+</tileset>
